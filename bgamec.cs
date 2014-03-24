d558a9abdcda5352e87e44a7d4b399c6f2a8134f
buzirat <alexbuz.dd@gmail.com>
2014-03-24 19:47:01 +0100

    xxx

---
+++ bgamec.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bgamec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                
+
+
+        }
+
+        public static void WaitOnMove()
+        {
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+                if ((keyinfo.Key == ConsoleKey.UpArrow) || (keyinfo.Key == ConsoleKey.DownArrow) || (keyinfo.Key == ConsoleKey.LeftArrow) || (keyinfo.Key == ConsoleKey.RightArrow))
+                {
+
+                }
+
+            }
+
+            while (keyinfo.Key != ConsoleKey.X);    
+        }
+    }
+}
